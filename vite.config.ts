8c340dac07ea516156cb123887c21659a75a8afc
Yana <yanaburbitskaya@gmail.com>
2026-08-20 13:22:13 +0300

    fix github spa routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa"
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
+   plugins: [react(), tailwindcss(),
+    githubPagesSpa()],
    base: '/headless-menu/',   
 })
