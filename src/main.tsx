069f55e6f66ebe9aad4951a2c732b8ca0e327a98
Yana <yanaburbitskaya@gmail.com>
2026-08-20 13:56:31 +0300

    save basename to vite config

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
-const basename = import.meta.env.PROD ? '/headless-menu/' : '/';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
    plugins: [react(), tailwindcss()],
-   base: '/headless-menu/', 
+   base: process.env.NODE_ENV === 'production' ? '/headless-menu/' : '/', 
 })
