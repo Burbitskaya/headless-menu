5636df90d1933cdfd81e6a0653c03701851799d2
Yana <yanaburbitskaya@gmail.com>
2026-08-20 14:46:34 +0300

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
+const base = process.env.NODE_ENV === 'production' ? '/headless-menu/' : '/';
 
 // https://vite.dev/config/
 export default defineConfig({
    plugins: [react(), tailwindcss()],
-   base: '/headless-menu/', 
+   base, 
 })
