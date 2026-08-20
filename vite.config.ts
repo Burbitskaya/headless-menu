cc6d43cdb8d17a5967ea14d4459d57f8db9c1e1f
Yana <yanaburbitskaya@gmail.com>
2026-08-20 14:53:28 +0300

    add vite-plugin-github-pages-spa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
+import { githubPagesSpa } from '@sctg/vite-plugin-github-pages-spa'; 
 
 const base = process.env.NODE_ENV === 'production' ? '/headless-menu/' : '/';
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
+   plugins: [react(), tailwindcss(), githubPagesSpa()],
    base, 
 })
