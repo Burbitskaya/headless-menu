4a4a2a4af45b6179df9a8da86561f6be3a1cc154
Yana <yanaburbitskaya@gmail.com>
2026-08-20 14:26:19 +0300

    generate prod mod

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode })=> ({
    plugins: [react(), tailwindcss()],
-   base: process.env.NODE_ENV === 'production' ? '/headless-menu/' : '/', 
-})
+   base: mode === 'production' ? '/headless-menu/' : '/', 
+}))
